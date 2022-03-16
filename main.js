76e7b9f592e39b9de04e6d1997d2fc168ef0f55f
ikbenignace <ikbenignace@live.be>
2022-03-16 12:41:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://calorieninjas.com/api');
+    let count = 0;
+    page.on('response', async (response) => {    
+        if (response.url().startsWith('https://api.calorieninjas.com/v1/nutrition')){
+            if(count == 1) { 
+                let element = await page.$('#sample-response')
+                let value = await element.evaluate(el => el.textContent)
+                res.json(JSON.parse(value))
+                browser.close();
+            }
+            count++
+        } 
+    }); 
+    await page.$eval('#sample-api-request', el => el.value = '');
+    await page.type('#sample-api-request', '200g chicken')
+    await page.click('#submitSampleAPIRequest')
+})
+
+app.listen(port, () => {
+    console.log(`API listening on port ${port}`)
+});
